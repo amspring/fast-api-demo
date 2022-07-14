7158f31785d56fee40ef3dbf52c5b9c6f4f9f8c2
chenshuchuan <chenshuchuan@xroom.net>
2022-07-14 17:41:09 +0800

    docs config.yml change

--- boot.sh
+++ boot.sh
@@ -7,16 +7,13 @@ typeset -l ENV
 ENV=$PROJECT_ENV
 cp "config/config.${ENV}.yml" config.yml
 
-echo "remove all log..."
-rm -rf *log*
-
 echo "install requirements"
 /opt/python3.10.2/bin/python3.10 -m pip install --no-cache-dir -i https://pypi.tuna.tsinghua.edu.cn/simple -r requirements.txt
 
 echo "copy api.ini to /var/app/conf/supervisord.d"
 cp config/api.ini /var/app/conf/supervisord.d
 
-echo "start service"
+echo "update and restart service"
 supervisorctl update && supervisorctl restart api
 
-echo "enjoy!"
+echo "Success!"
